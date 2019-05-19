aeed1f3022eae7a6b262f9e4238d8397e5e32817
Timofey Tipishev <tipishev@gmail.com>
2019-05-19 22:11:33 +0200

    add a my_tuple_to_list from StackOverflow

--- chapter_4/lib_misc.erl
+++ chapter_4/lib_misc.erl
@@ -1,5 +1,11 @@
 -module(lib_misc).
--export([permutations/1]).
+-export([permutations/1, my_tuple_to_list/1]).
 
 permutations([]) -> [[]];
 permutations(L) -> [[H|T] || H <- L, T <- permutations(L--[H])].
+
+% my_tuple_to_list({A, B, C}) -> [A, B, C].
+my_tuple_to_list(T) -> my_tuple_to_list(T, 1, tuple_size(T)).
+my_tuple_to_list(T, Pos, Size) when Pos =< Size -> [element(Pos, T) | my_tuple_to_list(T, Pos+1, Size)];
+my_tuple_to_list(_T, _Pos, _Size) -> [].
+
