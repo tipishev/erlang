f0d53d926909eebd263a301a46570c7a4918c998
Timofey Tipishev <tipishev@gmail.com>
2019-05-20 10:27:38 +0200

    add my_time_func

--- chapter_4/lib_misc.erl
+++ chapter_4/lib_misc.erl
@@ -1,5 +1,5 @@
 -module(lib_misc).
--export([permutations/1, my_tuple_to_list/1]).
+-export([permutations/1, my_tuple_to_list/1, my_time_func/1]).
 
 permutations([]) -> [[]];
 permutations(L) -> [[H|T] || H <- L, T <- permutations(L--[H])].
@@ -9,3 +9,5 @@ my_tuple_to_list(T) -> my_tuple_to_list(T, 1, tuple_size(T)).
 my_tuple_to_list(T, Pos, Size) when Pos =< Size -> [element(Pos, T) | my_tuple_to_list(T, Pos+1, Size)];
 my_tuple_to_list(_T, _Pos, _Size) -> [].
 
+my_time_func(F) -> my_time_func(F, erlang:system_time(microsecond)).
+my_time_func(F, StartTime) -> {F(), erlang:system_time(microsecond) - StartTime}.
