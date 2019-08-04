2749442e6a559cfe3642d2c0fde959b00fde1bbb
Timofey Tipishev <tipishev@gmail.com>
2019-08-04 17:41:48 +0200

    add chapter 9 dir

---
+++ chapter9/walks.erl
@@ -0,0 +1,7 @@
+-module(walks).
+-export([plan_route/2]).
+
+-spec plan_route(From:: point(), To:: point()) -> route().
+-type direction() :: north | south | east | west.
+-type point() :: {integer(), integer()}.
+-type route() :: [{go, direction(), integer()}].
