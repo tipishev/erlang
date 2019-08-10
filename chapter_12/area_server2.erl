6286a4c5c7dc63d1f6521cb3b1b530d15c711fb3
Timofey Tipishev <tipishev@gmail.com>
2019-08-11 00:50:04 +0200

    add area_server examples from chapter 12

---
+++ chapter_12/area_server2.erl
@@ -0,0 +1,24 @@
+-module(area_server2).
+-export([loop/0, rpc/2]).
+
+rpc(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+        {Pid, Response} ->
+            Response
+    end.
+
+loop() ->
+    receive
+        {From, {rectangle, Width, Height}} ->
+            From ! {self(), Width * Height},
+            loop();
+        {From, {square, Side}} ->
+            From ! {self(), Side * Side},
+            loop();
+        {From, {circle, Radius}} ->
+            From ! {self(), 3.141592 * Radius * Radius},
+            loop();
+        {From, Other} ->
+            From ! {self(), {error, Other}}
+    end.
