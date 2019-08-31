39139b23f291889d3513bedede17123640defb67
Timofey Tipishev <tipishev@gmail.com>
2019-08-31 22:56:41 +0200

    add build stages

--- theory/world.erl
+++ theory/world.erl
@@ -1,8 +1,17 @@
 -module(world).
 % -export([hello/0]).
--export([add/2]).
+-export([
+         hello/0,
+         add/2
+         % macrofoo/0
+        ]).
+
+% -define(p, o, o]).
 
 % -include("world.hrl").
 
 % hello() -> ?GREETING.
+hello() -> "wazzup".
 add(A, B) -> A + B.
+
+% macrofoo() -> [f, ?p.
