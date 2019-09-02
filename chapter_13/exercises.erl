a4a48a4594f3a0a1ea0365384c9f481a5ef39b5d
Timofey Tipishev <tipishev@gmail.com>
2019-09-02 07:08:41 +0200

    add red shirt function

--- chapter_13/exercises.erl
+++ chapter_13/exercises.erl
@@ -1,8 +1,22 @@
 -module(exercises).
--export([my_spawn/3]).
+-compile(export_all).
+
+run() ->
+    my_spawn(exercises, red_shirt, []).
+
+print(S) -> io:format("~p~n", [S]).
 
 my_spawn(Mod, Func, Args) ->
-    spawn(Mod, Func, Args).
+    statistics(runtime),
+    Pid = spawn(Mod, Func, Args),
+    {_, Time} = statistics(runtime),
+    io:format("it lived for ~p ms~n", [Time]),
+    Pid.
 
-red_shirt(Any) ->
-    list_to_atom(Any).
+red_shirt() ->
+    receive
+        bullet -> error('I am dead lol');
+        _ ->
+            print("still alive"),
+            red_shirt()
+    end.
