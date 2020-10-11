4e0d0a2147e331fd34971ca0c4fe31ac164a1ff8
Timofey Tipishev <tipishev@gmail.com>
2020-10-11 23:24:26 +0200

    add ex 16.1

--- programming_erlang/chapter_16/exercises.erl
+++ programming_erlang/chapter_16/exercises.erl
@@ -1,13 +1,22 @@
 -module(exercises).
 
 -export([ex1/0]).
+-import(filelib, [last_modified/1]).
 
 ex1() -> check_recompile(?MODULE).
 
 check_recompile(Module) ->
-    ErlFile = io:format("~p.erl~n", [Module]),
-    BeamFile = io:format("~p.beam~n", [Module]),
-    filelib:last_modified(ErlFile).
+    Erl = concat(Module, ".erl"),
+    Beam = concat(Module, ".beam"),
+    last_modified(Erl) > last_modified(Beam).
+
+concat(A, B) when is_atom(A) ->
+    A1 = erlang:atom_to_list(A),
+    concat(A1, B);
+concat(A, B) when is_list(A), is_list(B) ->
+    unicode:characters_to_list([A, B]).
+
+
 
 
 
