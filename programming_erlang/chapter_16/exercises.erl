2adb4da4dfaf75549c98d3c7a2f7cba672c53c1b
Timofey Tipishev <tipishev@gmail.com>
2020-10-13 15:16:09 +0200

    copy count_characters from the book, step on a bug

--- programming_erlang/chapter_16/exercises.erl
+++ programming_erlang/chapter_16/exercises.erl
@@ -1,14 +1,43 @@
 -module(exercises).
 
--export([ex1/0, ex2/0, ex3/0]).
+-export([ex1/0, ex2/0, ex3/0, ex4/0]).
+
 -import(filelib, [last_modified/1]).
 -import(string, [to_lower/1]).
+
+-import(lib_find, [files/3]).
 
 ex1() -> check_recompile(?MODULE).
 ex2() -> file_md5("lib_find.erl").
 ex3() -> big_file_md5("/home/user/Downloads/archlinux-2020.10.01-x86_64.iso").
+% ex4() -> find_dupes(".").
+ex4() -> count_characters("kalendula").
 
 %% underhood
+
+% find_dupes(Dir) ->
+%     Filenames = files(Dir, "*.beam", true),
+%     FilesAndMd5s = lists:map(fun(Filename) -> {Filename, file_md5(Filename)} end, Filenames),
+%     find_dupes(FilesAndMd5s, #{}).
+
+% find_dupes([H|T], #{ H => N}=X) ->
+%     io:format("~p~p~p~p~n", [H, T, N, X]),
+%     42;
+%     % {Md5, Dupes} = H,
+%     % find_dupes(T, X#{ Md5 := [Filename | Dupes]  }).
+% find_dupes([], X) -> X.
+
+count_characters(Str) ->
+   count_characters(Str, #{}).
+
+count_characters([H|T], #{ H := N }=X) ->
+   count_characters(T, X#{ H := N+1 });
+
+count_characters([H|T], X) ->
+   count_characters(T, X#{ H => 1 });
+
+count_characters([], X) ->
+        X.
 
 big_file_md5(Filename) ->
     Context = erlang:md5_init(),
