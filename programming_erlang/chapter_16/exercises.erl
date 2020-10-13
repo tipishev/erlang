1c245a6936c29e2c2b901c12aa48d97bd390e1f9
Timofey Tipishev <tipishev@gmail.com>
2020-10-13 11:41:23 +0200

    add ex 16.3

--- programming_erlang/chapter_16/exercises.erl
+++ programming_erlang/chapter_16/exercises.erl
@@ -1,13 +1,32 @@
 -module(exercises).
 
--export([ex1/0, ex2/0]).
+-export([ex1/0, ex2/0, ex3/0]).
 -import(filelib, [last_modified/1]).
 -import(string, [to_lower/1]).
 
 ex1() -> check_recompile(?MODULE).
 ex2() -> file_md5("lib_find.erl").
+ex3() -> big_file_md5("/home/user/Downloads/archlinux-2020.10.01-x86_64.iso").
 
 %% underhood
+
+big_file_md5(Filename) ->
+    Context = erlang:md5_init(),
+    {ok, File} = file:open(Filename, [read, binary, raw]),
+    md5_in_chunks(Context, File, _ChunkSize=4096, _Offset=0).
+
+md5_in_chunks(Context, File, ChunkSize, Offset) ->
+    ChunkOrEof = file:pread(File, Offset, ChunkSize),
+    case ChunkOrEof of
+        {ok, Data} ->
+            NewContext = erlang:md5_update(Context, Data),
+            md5_in_chunks(NewContext, File, ChunkSize, Offset + ChunkSize);
+        eof ->
+            <<Int:128>> = erlang:md5_final(Context),
+            to_lower(integer_to_list(Int, 16))
+    end.
+
+
 
 file_md5(Filename) ->
     case file:read_file(Filename) of
@@ -29,8 +48,3 @@ concat(A, B) when is_atom(A) ->
     concat(A1, B);
 concat(A, B) when is_list(A), is_list(B) ->
     unicode:characters_to_list([A, B]).
-
-
-
-
-
