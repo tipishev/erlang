b92e70c3a219be3e66431dc1bbcb6e4356510096
Timofey Tipishev <tipishev@gmail.com>
2020-10-20 13:36:06 +0200

    add broadcast example

--- programming_erlang/chapter_17/broadcast.erl
+++ programming_erlang/chapter_17/broadcast.erl
@@ -2,4 +2,23 @@
 -export([send/1, listen/0]).
 
 send(IoList) ->
-    case inet:ifget("wlp2s0", [broaddr
+    case inet:ifget("wlp2s0", [broadaddr])  of
+        {ok, [{broadaddr, Ip}]} ->
+            {ok, S} = gen_udp:open(5010, [{broadcast, true}]),
+            gen_udp:send(S, Ip, 6000, IoList),
+            gen_udp:close(S);
+        _ ->
+            io:format("Bad interface name, or\n"
+                      "broadcasting is not supported")
+    end.
+
+listen() ->
+    {ok, _} = gen_udp:open(6000),
+    loop().
+
+loop() ->
+    receive
+        Any ->
+            io:format("received:~p~n", [Any]),
+            loop()
+    end.
