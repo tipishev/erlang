266d915217edd1b3373505ac9f3c01c9650d5bdb
Timofey Tipishev <tipishev@gmail.com>
2020-10-31 11:11:20 +0000

    add file_index changes

--- programming_erlang/chapter_19/lib_filenames_dts.erl
+++ programming_erlang/chapter_19/lib_filenames_dts.erl
@@ -1,18 +1,18 @@
 -module(lib_filenames_dts).
 -export([
     open/1,
-    close/0
+    close/0,
     % test/0,
-    % filename2index/1,
+    filename2index/1
     % index2filename/1,
 ]).
 
 open(Filename) ->
     io:format("dets opened:~p~n", [Filename]),
-    IsFile = filelib:is_file(Filename),
+    IsExistingFile = filelib:is_file(Filename),
     case dets:open_file(?MODULE, [{file, Filename}]) of
         {ok, ?MODULE} ->
-            case IsFile of
+            case IsExistingFile of
                 true -> void;
                 false -> ok = dets:insert(?MODULE, {free, 1})
             end,
@@ -23,3 +23,6 @@ open(Filename) ->
     end.
 
 close() -> dets:close(?MODULE).
+
+filename2index(Filename) when is_binary(Filename) ->
+    case dets:lookup(?MODULE, Filename) of
