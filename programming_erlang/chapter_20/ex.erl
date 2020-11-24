b7df268f7ea6cf816eb8d33068569c3398e513c5
Timofey Tipishev <tipishev@gmail.com>
2020-11-24 11:37:03 +0000

    add some mnesia scratching

--- programming_erlang/chapter_20/ex.erl
+++ programming_erlang/chapter_20/ex.erl
@@ -1,6 +1,16 @@
 -module(ex).
 
+-export([init_db/0]).
+
 % ex1
 -record(user, {name, email, password}).
 -record(tip, {url, description, review_date}).
--record(abuse, user, ip, tips_today).
+-record(abuse, {name, ip, tips_today}).
+
+init_db() ->
+    mnesia:create_schema([node()]),
+    mnesia:start(),
+    mnesia:create_table(users, [{attributes, record_info(fields, user)}]),
+    mnesia:create_table(tips, [{attributes, record_info(fields, tip)}]),
+    mnesia:create_table(abuses, [{attributes, record_info(fields, abuse)}]),
+    mnesia:stop().
